a04ebfe4dcd37d0efbbc8f552eca94182958232c
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-06-09 03:36:15 +0200

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sahlhaly_event_planner/style.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,17 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -30,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -46,68 +33,171 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Color(0XFF3F51b5),
+
+      // persist create account link at the bottom of screen
+      bottomNavigationBar: _createAccountLink(),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 70,
+          ),
+          _topheader(),
+          Expanded(
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 32),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                  color: Colors.grey[50],
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 40),
+                      _labelText('Email:'),
+                      _inputTextField('example@email.com', false),
+                      SizedBox(height: 16),
+                      _labelText('Password:'),
+                      _inputTextField('******', true),
+                      SizedBox(height: 12),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: InkWell(
+                          onTap: () {
+                            //TODO
+                          },
+                          child: Text(
+                            'Forgot Password ?',
+                            style: TextStyle(
+                              color: Colors.blue[900],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 46,
+                          width: 160,
+                          child: RaisedButton(
+                            onPressed: () {
+                              //TODO
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            color: Color(0XFF303f9f),
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+
+                    ],
+                  ),
+                ),
+              )),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  _createAccountLink() {
+    return Container(
+      color: Colors.grey[50],
+      padding: EdgeInsets.only(bottom: 16),
+      height: 60,
+      child: Center(
+        child: Text(
+          'Don\'t have account ? Sign Up Now',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: facebookColor,
+            fontSize: 16,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  //button to login in using scial media,
+
+
+  _inputTextField(hintText, bool obscuretext) {
+    return Container(
+      height: 56,
+      padding: EdgeInsets.fromLTRB(16, 3, 16, 6),
+      margin: EdgeInsets.all(8),
+      decoration: raisedDecoration,
+      child: Center(
+        child: TextField(
+          obscureText: obscuretext,
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hintText,
+              hintStyle: TextStyle(
+                color: Colors.black38,
+              )),
+        ),
+      ),
+    );
+  }
+
+  _labelText(title) {
+    return Padding(
+      padding: EdgeInsets.only(left: 24),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          color: Colors.black,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  _topheader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Create\nYour\nAccount',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 28,
+            ),
+          ),
+          Image.asset(
+            'assets/gbimage.png',
+            height: 170,
+            fit: BoxFit.fitHeight,
+          )
+        ],
+      ),
     );
   }
 }
