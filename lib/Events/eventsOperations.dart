cecad26e07bce305d443ea852b457222209f7c09
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-06-25 16:40:32 +0200

    admin logic done and admin drawer

--- lib/Events/eventsOperations.dart
+++ lib/Events/eventsOperations.dart
@@ -2,39 +2,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sahlhaly_event_planner/models/event_model.dart';
 
 class Eoboperation {
-  final Firestore _firestore = Firestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   addJob(EventModel eventModel) {
     _firestore.collection('Events').add({
-      'Job Nature':jobsHot.JobNature,
-      'Job Categories':jobsHot.JobCat,
-      'FirmID':jobsHot.firmid,
-      'FirmName': jobsHot.companyName,
-      'JobTitle': jobsHot.title,
-      'JobRequirements': jobsHot.requirement,
-      'Salary': jobsHot.salary,
-      'Deadline': jobsHot.deadline,
-      'JobDescription': jobsHot.description,
-      'Education': jobsHot.education,
-      'Imageurl': jobsHot.image,
-      'Location': jobsHot.location,
-      'Vacancy': jobsHot.vacancies
+      'EventName':eventModel.eventName,
+      'EventCategories':eventModel.eventCat,
+      'AudinceNum':eventModel.audNum,
+      'EventAds': eventModel.eventAds,
+      'EventBudget ': eventModel.eventBudget,
+      'EventEqu': eventModel.eventEqu,
+      'Time': eventModel.eventTime,
+      'Deadline': eventModel.deadline,
+      'EventIdea': eventModel.eventIdea,
+      'Location': eventModel.location,
+      'Type': eventModel.type
     });
   }
 
-  Stream<QuerySnapshot> loadvents() {
+  Stream<QuerySnapshot> loadevents() {
     return _firestore.collection('Events').snapshots();
   }
 
 
 
-  deleteJob(documentId){
-    _firestore.collection('Events').document(documentId).delete();
+  deleteevent(documentId){
+    _firestore.collection('Events').doc(documentId).delete();
 
   }
 
-  editJob(data,documentId){
-    _firestore.collection('Events').document(documentId).updateData(data);
+  editevent(data,documentId){
+    _firestore.collection('Events').doc(documentId).update(data);
   }
 
 }
