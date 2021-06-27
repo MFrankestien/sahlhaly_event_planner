c36fb039f41f001c9a8f6f4505b06477ca6c667f
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-06-27 17:06:22 +0200

    admin butt

--- lib/Component/drawer.dart
+++ lib/Component/drawer.dart
@@ -42,118 +42,124 @@ class _MainDrawerState extends State<MainDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        child: Padding(
-          padding: EdgeInsets.only(top: 50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          decoration: BoxDecoration( gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors:  [Color(0xff1a84b8), Color(0xff2b6ff7)])),
+          child: Padding(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
 
-              CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage:(image!=null)?NetworkImage(image):NetworkImage(
-                    'https://i.stack.imgur.com/l60Hf.png',
-              )),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                "$name $lname",
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w800,
+                CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage:(image!=null)?NetworkImage(image):NetworkImage(
+                      'https://i.stack.imgur.com/l60Hf.png',
+                )),
+                SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
+                Text(
+                  "$name $lname",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
 
-              Divider(
-                color: Colors.blue,
-                thickness: 2.0,
-              ),
-            ],
+                Divider(
+                  color: Colors.blue,
+                  thickness: 1.0,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      //Now let's Add the button for the Menu
-      //and let's copy that and modify it
-      ListTile(
-        onTap: () {
-          Navigator.push(
+        SizedBox(
+          height: 20.0,
+        ),
+        //Now let's Add the button for the Menu
+        //and let's copy that and modify it
+        ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PersonalDetail(
+                    )));
+
+          },
+          leading: Icon(
+            Icons.person,
+            color: Colors.black,
+          ),
+          title: Text("Your Profile"),
+        ),
+
+        ListTile(
+          onTap: () {
+            Navigator.push(
               context,
-              MaterialPageRoute(
-                  builder: (context) => PersonalDetail(
-                  )));
-
-        },
-        leading: Icon(
-          Icons.person,
-          color: Colors.black,
+              MaterialPageRoute(builder: (context) => MasterCard()),
+            );
+          },
+          leading: Icon(
+            Icons.credit_card,
+            color: Colors.black,
+          ),
+          title: Text("Manage payment info"),
         ),
-        title: Text("Your Profile"),
-      ),
 
-      ListTile(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MasterCard()),
-          );
-        },
-        leading: Icon(
-          Icons.credit_card,
-          color: Colors.black,
+        ListTile(
+          onTap: () {},
+          leading: Icon(
+            Icons.info,
+            color: Colors.black,
+          ),
+          title: Text("About Us"),
         ),
-        title: Text("Manage payment info"),
-      ),
 
-      ListTile(
-        onTap: () {},
-        leading: Icon(
-          Icons.info,
-          color: Colors.black,
+        ListTile(
+          onTap: () {},
+          leading: Icon(
+            Icons.mail,
+            color: Colors.black,
+          ),
+          title: Text("Contact Us"),
         ),
-        title: Text("About Us"),
-      ),
 
-      ListTile(
-        onTap: () {},
-        leading: Icon(
-          Icons.mail,
-          color: Colors.black,
+        ListTile(
+          onTap: () {},
+          leading: Icon(
+            Icons.settings,
+            color: Colors.black,
+          ),
+          title: Text("Settings"),
         ),
-        title: Text("Contact Us"),
-      ),
 
-      ListTile(
-        onTap: () {},
-        leading: Icon(
-          Icons.settings,
-          color: Colors.black,
+        ListTile(
+          onTap: () {
+            AuthService().signOut();
+            Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                Wrapper()), (Route<dynamic> route) => false);
+          },
+          leading: Icon(
+            Icons.logout,
+            color: Colors.black,
+          ),
+          title: Text("LogOut"),
         ),
-        title: Text("Settings"),
-      ),
-
-      ListTile(
-        onTap: () {
-          AuthService().signOut();
-          Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-              Wrapper()), (Route<dynamic> route) => false);
-        },
-        leading: Icon(
-          Icons.logout,
-          color: Colors.black,
-        ),
-        title: Text("LogOut"),
-      ),
 
 
-    ]);
+      ]),
+    );
   }
 }
